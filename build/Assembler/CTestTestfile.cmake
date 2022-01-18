b4108a82d372d08250989b088be11c0ddc9a557b
Ronald Veldema <rsveldema@gmail.com>
2022-01-18 23:51:41 +0100

    remove all these generated files from the repo.

--- build/Assembler/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rove/source/repos/ecc/ECC_CPU/Assembler
-# Build directory: C:/Users/rove/source/repos/ecc/ECC_CPU/build/Assembler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rove/source/repos/ecc/ECC_CPU
-# Build directory: C:/Users/rove/source/repos/ecc/ECC_CPU/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CoreLib")
-subdirs("Assembler")
-subdirs("Simulator")
